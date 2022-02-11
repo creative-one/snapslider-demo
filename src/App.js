25b7089e2cdb9eea8677df738ed0ba15fe82f4ff
mz@360vier.de <mz@360vier.de>
2022-02-11 10:17:18 +0100

    set test env

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import './App.css';
-import SnapSlider from 'snapslider-react';
+import SnapSlider from "snapslider-react";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Slider Test</h1>
-        <SnapSlider />
-    </div>
-  );
+    const images = useRandomImages()
+    const [count, setCount] = useState(2)
+
+    return (
+        <div className="App">
+            <h1>SnapSlider v1</h1>
+            <Section title={(<>
+                <span>Bilderslider</span>
+                <input
+                    type={"range"}
+                    min={1}
+                    max={12}
+                    value={count}
+                    onChange={e => setCount(e.target.value)}
+                />
+            </>)}>
+                <SnapSlider
+                    groupSize={'100%'}
+                    itemsPerGroup={count}
+                    onUpdateSettings={({itemsPerGroup}) => setCount(itemsPerGroup)}
+                >
+                    {images.map(image => <img key={image.id} src={image.download_url} alt={image.author}/>)}
+                </SnapSlider>
+            </Section>
+        </div>
+    );
+}
+
+const Section = ({title, children}) => {
+    return (
+        <div className={'section'}>
+            <h2>{title}</h2>
+            {children}
+        </div>
+    )
+}
+
+const useRandomImages = (limit = 10) => {
+    const [images, setImages] = useState([])
+    useEffect(() => {
+        fetch('https://picsum.photos/v2/list?limit=' + limit).then(resp => resp.json()).then(data => {
+            setImages(data)
+        })
+    }, [limit])
+
+    return images
 }
 
 export default App;
